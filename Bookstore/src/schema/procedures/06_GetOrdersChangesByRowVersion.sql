58e61c302223090409971ed5b05bc4e97a0ae182
Kevin Padilla <padilla.kevin.19.06@gmail.com>
2026-08-09 21:09:23 -0400

    ETL FactOrders - GetBookChangesByRowVersion: cambiar INNER JOIN a LEFT JOIN para incluir libros sin autor, usar STRING_AGG para consolidar múltiples autores - GetOrdersChangesByRowVersion: agregar CTE con ROW_NUMBER para tomar solo el estado más reciente por orden y evitar duplicados

--- Bookstore/src/schema/procedures/06_GetOrdersChangesByRowVersion.sql
+++ Bookstore/src/schema/procedures/06_GetOrdersChangesByRowVersion.sql
@@ -1,13 +1,53 @@
-CREATE PROCEDURE [dbo].[GetOrdersChangesByRowVersion]
+CREATE OR ALTER PROCEDURE [dbo].[GetOrdersChangesByRowVersion]
 (
    @startRow BIGINT 
    ,@endRow  BIGINT 
 )
 AS
 BEGIN
+	WITH ChangedOrders AS (
+		SELECT DISTINCT co.order_id
+		FROM [dbo].[cust_order] co
+		INNER JOIN [dbo].[order_line] ol ON (co.order_id = ol.order_id)
+		INNER JOIN [dbo].[order_history] oh ON (co.order_id = oh.order_id)
+		INNER JOIN [dbo].[order_status] os ON (oh.status_id = os.status_id)
+		WHERE (co.[rowversion] > CONVERT(ROWVERSION,@startRow) AND co.[rowversion] <= CONVERT(ROWVERSION,@endRow))
+		OR (ol.[rowversion] > CONVERT(ROWVERSION,@startRow) AND ol.[rowversion] <= CONVERT(ROWVERSION,@endRow))
+		OR (oh.[rowversion] > CONVERT(ROWVERSION,@startRow) AND oh.[rowversion] <= CONVERT(ROWVERSION,@endRow))
+		OR (os.[rowversion] > CONVERT(ROWVERSION,@startRow) AND os.[rowversion] <= CONVERT(ROWVERSION,@endRow))
+	),
+	LatestOrderHistory AS (
+		SELECT 
+			oh.order_id,
+			oh.history_id,
+			oh.status_id,
+			oh.status_date,
+			ROW_NUMBER() OVER (PARTITION BY oh.order_id ORDER BY oh.status_date DESC, oh.history_id DESC) AS rn
+		FROM [dbo].[order_history] oh
+		WHERE oh.order_id IN (SELECT order_id FROM ChangedOrders)
+	)
 	SELECT 
 	co.[order_id]
-	,co.[order_date]
+	,OrderDateKey = CONVERT(INT,
+							(CONVERT(CHAR(4),DATEPART(YEAR,co.[order_date]))
+						  + CASE 
+								WHEN DATEPART(MONTH,co.[order_date]) < 10 THEN '0' + CONVERT(CHAR(1),DATEPART(MONTH,co.[order_date]))
+								ELSE + CONVERT(CHAR(2),DATEPART(MONTH,co.[order_date]))
+							END
+						  + CASE 
+								WHEN DATEPART(DAY,co.[order_date]) < 10 THEN '0' + CONVERT(CHAR(1),DATEPART(DAY,co.[order_date]))
+								ELSE + CONVERT(CHAR(2),DATEPART(DAY,co.[order_date]))
+							END))
+	,StatusDateKey = CONVERT(INT,
+							(CONVERT(CHAR(4),DATEPART(YEAR,oh.[status_date]))
+						  + CASE 
+								WHEN DATEPART(MONTH,oh.[status_date]) < 10 THEN '0' + CONVERT(CHAR(1),DATEPART(MONTH,oh.[status_date]))
+								ELSE + CONVERT(CHAR(2),DATEPART(MONTH,oh.[status_date]))
+							END
+						  + CASE 
+								WHEN DATEPART(DAY,oh.[status_date]) < 10 THEN '0' + CONVERT(CHAR(1),DATEPART(DAY,oh.[status_date]))
+								ELSE + CONVERT(CHAR(2),DATEPART(DAY,oh.[status_date]))
+							END))
 	,co.[customer_id]
 	,co.[shipping_method_id]
 	,co.[dest_address_id]
@@ -15,14 +55,12 @@ BEGIN
 	,ol.[book_id]
 	,ol.[price]
 	,oh.[history_id]
-	,oh.[status_date]
 	,os.[status_value]
-	FROM [dbo].[cust_order] co
+	,co.[order_date]
+	,oh.[status_date]
+	FROM ChangedOrders ca
+	INNER JOIN [dbo].[cust_order] co ON (ca.order_id = co.order_id)
 	INNER JOIN [dbo].[order_line] ol ON (co.order_id = ol.order_id)
-	INNER JOIN [dbo].[order_history] oh ON (co.order_id = oh.order_id)
+	INNER JOIN LatestOrderHistory oh ON (co.order_id = oh.order_id AND oh.rn = 1)
 	INNER JOIN [dbo].[order_status] os ON (oh.status_id = os.status_id)
-	WHERE (co.[rowversion] > CONVERT(ROWVERSION,@startRow) AND co.[rowversion] <= CONVERT(ROWVERSION,@endRow))
-	OR (ol.[rowversion] > CONVERT(ROWVERSION,@startRow) AND ol.[rowversion] <= CONVERT(ROWVERSION,@endRow))
-	OR (oh.[rowversion] > CONVERT(ROWVERSION,@startRow) AND oh.[rowversion] <= CONVERT(ROWVERSION,@endRow))
-	OR (os.[rowversion] > CONVERT(ROWVERSION,@startRow) AND os.[rowversion] <= CONVERT(ROWVERSION,@endRow))
 END
